fc6ad845a32e00b90ef670ea8dd67d7727eac736
gvukamolle <tim.grusshko7@gmail.com>
2026-05-19 16:15:22 +0300

    chore: fix scaffold lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,29 @@
-import next from 'eslint-config-next'
+import tsParser from '@typescript-eslint/parser'
 
-export default [...next]
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      'payload-types.ts'
+    ]
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  }
+]
